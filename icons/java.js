68359cbc0b695c424edd5c4d3d5c15a2c0897b43
ajay96 <maroju.ajay47@gmail.com>
2020-10-22 08:35:11 +0530

    first commit

---
+++ icons/java.js
@@ -0,0 +1,11 @@
+var videos=[]
+
+function loadvideos(params) {
+    
+    var newsrc="https://mega.nz/embed/ZSg3gCoR#l-VvpVK2LnhQuqHVT5Sr2DZRVh6Namj4NJCRLxYhKZQ"
+    var newsrc1="https://mega.nz/embed/MTI3UAqR#S41CBNVUKrIw5Zo-NQpWa0iigwin3bwsO-YjNerhhpg"
+
+    // assigning value
+    document.getElementById("myFrame").src = newsrc1; 
+
+}
